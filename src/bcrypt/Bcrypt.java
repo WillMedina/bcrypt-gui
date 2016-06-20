4084388f86c4de608c78827984f7090e626405f3
will <will@dev>
2016-06-20 10:48:33 -0500

    Version 1

---
+++ src/bcrypt/Bcrypt.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcrypt;
+
+import gui.gui;
+
+/**
+ *
+ * @author will
+ */
+public class Bcrypt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        gui g = new gui();
+        g.setTitle("Convertir y comprobar cadenas BCrypt");
+        g.setLocationRelativeTo(null);
+        g.setVisible(true);
+    }
+    
+}
